4d19136df02d8b2308f1cfba68b864a490b0698a
Nguyen Van Tien <ti3n120903@gmail.com>
2026-03-20 16:07:52 +0700

    Revert "Initial commit: Add ShopTi ecommerce project"
    This reverts commit 4a29c845d41c60af5279b35d864d2db3e83740d8.

--- backend/routes-mongo/categories.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('../controllers-mongo/categories.controller');
-
-const router = express.Router();
-router.get('/', controller.list);
-
-module.exports = router;
-
